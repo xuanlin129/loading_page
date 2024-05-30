8e598821c2f8cb7645e91a2d151bd4de37894fca
Xuan <127477394+xuanlin129@users.noreply.github.com>
2024-05-30 10:44:52 +0800

    update safari 100vh

--- js/index.js
+++ js/index.js
@@ -12,3 +12,13 @@ window.addEventListener('load', () => {
     loadingBox.style.display = 'none'
   }, 2000)
 })
+
+function safariHacks() {
+  let windowsVH = window.innerHeight / 100;
+  document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
+  window.addEventListener('resize', function() {
+      document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
+  });
+}
+
+safariHacks();
